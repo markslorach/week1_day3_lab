2670bc92f802326d854bc1607fd5d103b0a63ccb
Mark Slorach <hello@markslorach.com>
2023-05-17 15:37:26 +0100

    5 tests passed

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -15,3 +15,8 @@ def subtract(number_1, number_2):
 def multiply(number_1, number_2):
     total = number_1 * number_2
     return total
+
+#test4
+def divide(number_1, number_2):
+    total = number_1 / number_2
+    return total
